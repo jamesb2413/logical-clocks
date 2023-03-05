eb2de03161f3f5ed1fb41b4f01813fc3b9136144
James Barna <jamesbarna@college.harvard.edu>
2023-03-04 21:53:29 -0500

    add sends to multiple processes (unknown indent error)

--- processes.py
+++ processes.py
@@ -23,39 +23,46 @@ def consumer(conn):
         net_q.append(dataVal)
  
 
-def producer(portVal):
+def producer(portVal1, portVal2):
     host= "127.0.0.1"
-    port = int(portVal)
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    port1 = int(portVal1)
+    port2 = int(portVal2)
+    s1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sleepVal = 0.500
     #sema acquire
     try:
-        s.connect((host,port))
-        print("Client-side connection success to port val:" + str(portVal) + "\n")
- 
-        # while True:
-        #     pass
-        #     time.sleep(sleepVal)
-        #     # If code not set because queue is not empty
-        #     if code == -1:
-        #         continue
-        #     elif code == 1:
-        #         # send to one of the other machines a message that is the local logical clock time, 
-        #         # update it’s own logical clock, and update the log with the send, the system time, and the logical clock time
-        #         # s.send(codeVal.encode('ascii'))
-        #         pass
-        #     elif code == 2:
-        #         # send to other machine a message that is the local logical clock time, 
-        #         # update it’s own logical clock, and update the log with the send, the system time, and the logical clock time
-        #         pass
-        #     elif code == 3:
-        #         # send to both other machines a message that is the local logical clock time, 
-        #         # update it’s own logical clock, and update the log with the send, the system time, and the logical clock time
-        #         pass
-        #     else:
-        #         # treat the cycle as an internal event; 
-        #         # update the local logical clock, and log the internal event, the system time, and the logical clock value.
-        #         pass
+        s1.connect((host,port1))
+        s2.connect((host,port2))
+        while True:
+            time.sleep(sleepVal)
+            # If code not set because queue is not empty
+            if code == -1:
+                continue
+            elif code == 1:
+                # send to one of the other machines a message that is the local logical clock time, 
+                # update it’s own logical clock, and update the log with the send, the system time, and the logical clock time
+                msg = "hello"
+                print("Client-side connection success to port val:" + str(portVal1) + "\n")
+                s1.send(msg.encode('ascii'))
+            elif code == 2:
+                # send to other machine a message that is the local logical clock time, 
+                # update it’s own logical clock, and update the log with the send, the system time, and the logical clock time
+                msg = "hello"
+                print("Client-side connection success to port val:" + str(portVal2) + "\n")
+                s2.send(msg.encode('ascii'))
+            elif code == 3:
+                # send to both other machines a message that is the local logical clock time, 
+                # update it’s own logical clock, and update the log with the send, the system time, and the logical clock time
+                msg = "hello"
+                print("Client-side connection success to port val:" + str(portVal1) + "\n")
+                print("Client-side connection success to port val:" + str(portVal2) + "\n")
+                s1.send(msg.encode('ascii'))
+                s2.send(msg.encode('ascii'))
+            else:
+                # treat the cycle as an internal event; 
+                # update the local logical clock, and log the internal event, the system time, and the logical clock value.
+                pass
 
     except socket.error as e:
         print ("Error connecting producer: %s" % e)
@@ -87,7 +94,7 @@ def machine(config):
     #add delay to initialize the server-side logic on all processes
     time.sleep(5)
     # extensible to multiple producers
-    prod_thread = Thread(target=producer, args=(config[2],))
+    prod_thread = Thread(target=producer, args=(config[2],config[3]))
     prod_thread.start()
     # Run clock cycles
     starttime = time.time()
@@ -113,11 +120,11 @@ if __name__ == '__main__':
     port3 = 4056
 
 
-    config1=[localHost, port1, port2]
+    config1=[localHost, port1, port2, port3]
     p1 = Process(target=machine, args=(config1,))
-    config2=[localHost, port2, port3]
+    config2=[localHost, port2, port3, port1]
     p2 = Process(target=machine, args=(config2,))
-    config3=[localHost, port3, port1]
+    config3=[localHost, port3, port1, port2]
     p3 = Process(target=machine, args=(config3,))
 
     p1.start()
